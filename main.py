9423e192e42534a9ca256df9238d710fda450fa0
jackpurrin <jackpurrin@proton.me>
2024-12-20 17:18:42 -0700

    lets commit a broken project!

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from cryptography.fernet import Fernet
+
 print("python passw0rd manager")
 print("not a safe password manager to use for daily use")
 print("use for testing only! no secrets!")
+
+print("")
+print("1.) Generate a key and encrypt a database")
+print("2.) Make a database")
+
+option = input("What do you want to do?")
+
+if option == 1 :
+    with open('filekey.key', 'wb') as filekey:
+    filekey.write(key)
+
+print("done")
+
